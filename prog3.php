21b66c0405ce65b331b7953abf2f917d4a67723b
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-06-10 13:04:41 +0530

    Add files via upload

---
+++ prog3.php
@@ -0,0 +1,10 @@
+<?php
+$a = 20;
+$b = 10;
+
+echo "addtion : ".($a + $b)."</br>";
+echo "subtraction : ".($a - $b)."</br>";
+echo "division : ".($a / $b)."</br>";
+echo "multiplication : ".($a * $b)."</br>";
+
+?>
