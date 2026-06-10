893857b1037018baada545f5ef50bd7795822175
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-06-10 13:03:58 +0530

    Add files via upload

---
+++ prog1.php
@@ -0,0 +1,15 @@
+<?php
+const university = "gujrat university";
+        
+$name = "raj kadecha";
+$semester = 4;
+$spi = "6.57";
+$result = "pass";
+
+echo "================ sem 4 result ================"."</br>";
+echo "university : ".university."</br>";
+echo"name : ".$name."</br>";
+echo"semester : ".$semester."</br>";
+echo"spi : ".$spi."</br>";
+echo"result : ".$result."</br>";
+?>
