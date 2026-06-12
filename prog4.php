e62d8e487a050539699eacde713249847bf117c8
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-06-12 12:18:40 +0530

    Add files via upload

---
+++ prog4.php
@@ -0,0 +1,28 @@
+<?php
+$month = date("n");
+
+if($month == 1)
+    echo "Current Month: January";
+else if($month == 2)
+    echo "Current Month: February";
+else if($month == 3)
+    echo "Current Month: March";
+else if($month == 4)
+    echo "Current Month: April";
+else if($month == 5)
+    echo "Current Month: May";
+else if($month == 6)
+    echo "Current Month: June";
+else if($month == 7)
+    echo "Current Month: July";
+else if($month == 8)
+    echo "Current Month: August";
+else if($month == 9)
+    echo "Current Month: September";
+else if($month == 10)
+    echo "Current Month: October";
+else if($month == 11)
+    echo "Current Month: November";
+else
+    echo "Current Month: December";
+?>
