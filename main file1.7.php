73acec832363363ba0777d5f0610a5a71a62c55f
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-07-01 10:57:50 +0530

    Add files via upload

---
+++ main file1.7.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h3>starting main file.....</h3>";
+
+// Using include
+echo "<b>Using include:</b><br>";
+include("file1-1.7.php");
+
+
+// Using require
+echo "<b>Using require:</b><br>";
+require("file2-1.7.php");
+
+echo "<br>end of main file<br>";
+
+?>
