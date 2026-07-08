b86cdc9ca208c48e3f3681f1114438429c11ad3c
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-07-08 10:46:44 +0530

    Add files via upload

---
+++ 2.4program.php
@@ -0,0 +1,20 @@
+<?php
+$name = "my name is raj.";
+
+echo strlen($name);
+echo "<br>";
+
+echo strpos($name,"a",0);
+echo "<br>";
+
+echo str_word_count($name);
+echo "<br>";
+
+echo strrev($name);
+echo "<br>";
+
+echo strtolower($name);
+echo "<br>";
+
+echo strtoupper($name);
+?>
