c831723b5b26a564e7f6ed8121d89522d440f25d
rajkadecha1 <rajkumar.kadecha127578@marwadiuniversity.ac.in>
2026-07-29 10:50:37 +0530

    Add files via upload

---
+++ program3.7.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+if(isset($_POST['login']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+    if($username == "admin" && $password == "1234")
+    {
+        $_SESSION['username'] = $username;
+        header("Location: homepage.php");
+        exit();
+    }
+    else
+    {
+        echo "Invalid Username or Password";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+</head>
+<body>
+
+<h2>Login Form</h2>
+
+<form method="post">
+    Username:
+    <input type="text" name="username" required><br><br>
+
+    Password:
+    <input type="password" name="password" required><br><br>
+
+    <input type="submit" name="login" value="Login">
+</form>
+
+</body>
+</html>
